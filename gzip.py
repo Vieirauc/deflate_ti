fda03e6dbea4780a73af9453f635ec201091fdde
PedroRamalho18 <pedroptramalho@gmail.com>
2022-12-13 10:07:29 +0000

    teste

--- gzip.py
+++ gzip.py
@@ -231,13 +231,15 @@ class GZIP:
 			self.available_bits -= n
 
 		return value
-	
+
+	#ex1
 	def read_block_format(self):
 		HLIT = self.readBits(5)
 		HDIST = self.readBits(5)
 		HCLEN = self.readBits(4)
 		return HLIT, HDIST, HCLEN
 
+	#teste para o joao ver
 	
 
 
